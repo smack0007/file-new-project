cac81543c898f59fe490890db51446dce82a117b
smack0007 <zachary.snow@gmail.com>
2020-09-08 21:36:23 +0200

    chalk and cloning a repo.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
-import { argv } from "process";
+import { argv, exit } from "process";
+import { red, green, blue } from "chalk";
+import { exec } from "shelljs";
 
-console.info("File->New->Project");
+console.info(`${red("File")}->${green("New")}->${blue("Project")}`);
 
-for (const arg of argv) {
-    console.info(arg);
+const args = [...argv];
+args.splice(0, 2);
+
+if (args.length === 0) {
+    console.error("Please provide the name of a GitHub repository to clone.");
+    exit(1);
 }
+
+exec(`git clone https://github.com/${args[0]}.git`);
